e23861f70db06cd011c223995166a7a6060353ba
Miyukiichan <sam.matthews19638@gmail.com>
2024-07-23 17:15:56 +0100

    Allow adding notes and lists + make home page the base list

--- listorize/Model/ListItem.cs
+++ listorize/Model/ListItem.cs
@@ -3,11 +3,13 @@ using listorize.Components.Util;
 namespace listorize.Model;
 
 public class ListItem : BaseModel {
-    public int ParentListId { get; set; }
+    [ForeignKey(nameof(ListItem.ParentList))]
+    public int? ParentListId { get; set; }
     [ForeignKey(nameof(ListItem.Note))]
     public int? NoteId { get; set; }
     [ForeignKey(nameof(ListItem.List))]
     public int? ListId { get; set; }
+    public List? ParentList { get; set; }
     public Note? Note { get; set; }
     public List? List { get; set; }
     public string? Name => List is not null ? List.Name : Note is not null ? Note.Name : "";
